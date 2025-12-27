0e1b60d31e666e192e0303835221b73b71a50f98
julia <julia@host.docker.internal>
2025-12-27 17:55:45 -0600

    feat: visualizar lista de colaboradores

--- src/controlpacketworld/FXMLColaboradoresController.java
+++ src/controlpacketworld/FXMLColaboradoresController.java
@@ -4,17 +4,28 @@
  */
 package controlpacketworld;
 
+import dominio.ColaboradorImp;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import pojo.Colaborador;
+import utilidad.Utilidades;
 
 /**
  * FXML Controller class
@@ -23,14 +34,60 @@ import javafx.stage.Stage;
  */
 public class FXMLColaboradoresController implements Initializable {
 
+    @FXML
+    private TableView<Colaborador> tvColaboradores;
+    @FXML
+    private TableColumn colNumPersonal;
+    @FXML
+    private TableColumn colNombre;
+    @FXML
+    private TableColumn colApellidoPaterno;
+    @FXML
+    private TableColumn colApellidoMaterno;
+    @FXML
+    private TableColumn colCurp;
+    @FXML
+    private TableColumn colCorreo;
+    @FXML
+    private TableColumn colRol;
+    @FXML
+    private TableColumn colSucursal;
+    
+    private ObservableList<Colaborador> colaboradores;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        configurarTabla();
+        cargarInformacionColaboradores();
+    }
 
+    private void configurarTabla() {
+        colNumPersonal.setCellValueFactory(new PropertyValueFactory("noPersonal"));
+        colNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
+        colApellidoPaterno.setCellValueFactory(new PropertyValueFactory("apellidoPaterno"));
+        colApellidoMaterno.setCellValueFactory(new PropertyValueFactory("apellidoMaterno"));
+        colCurp.setCellValueFactory(new PropertyValueFactory("curp"));
+        colCorreo.setCellValueFactory(new PropertyValueFactory("correo"));
+        colRol.setCellValueFactory(new PropertyValueFactory("rol"));
+        colSucursal.setCellValueFactory(new PropertyValueFactory("sucursal"));
+    }
+    
+    private void cargarInformacionColaboradores() {
+        HashMap<String, Object> respuesta = ColaboradorImp.obtenerTodos();
+        boolean esError = (boolean) respuesta.get("error");
+        if (!esError) {
+            List<Colaborador> colaboradoresAPI = (List<Colaborador>) respuesta.get("colaboradores");
+            colaboradores = FXCollections.observableArrayList();
+            colaboradores.addAll(colaboradoresAPI);
+            tvColaboradores.setItems(colaboradores);
+        } else {
+            Utilidades.mostrarAlertaSimple("Error al cargar", "" + respuesta.get("mensaje"), Alert.AlertType.NONE);
+        }
+    }
+ 
     @FXML
     private void tfBuscador(ActionEvent event) {
     }
--- src/dominio/ColaboradorImp.java
+++ src/dominio/ColaboradorImp.java
@@ -5,9 +5,14 @@
 package dominio;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import conexion.ConexionAPI;
 import dto.Respuesta;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import pojo.Colaborador;
 import pojo.RespuestaHTTP;
 import utilidad.Constantes;
@@ -17,6 +22,34 @@ import utilidad.Constantes;
  * @author julia
  */
 public class ColaboradorImp {
+    public static HashMap<String, Object> obtenerTodos() {
+        HashMap<String, Object> respuesta = new LinkedHashMap<>();
+        String URL = Constantes.URL_WS + "colaborador/obtener-todos";
+        RespuestaHTTP respuestaAPI = ConexionAPI.peticionGET(URL);
+        if (respuestaAPI.getCodigo() == HttpURLConnection.HTTP_OK) {
+            Gson gson = new Gson();
+            Type tipoLista = new TypeToken<List<Colaborador>>(){}.getType();
+            List<Colaborador> colaboradores = gson.fromJson(respuestaAPI.getContenido(), tipoLista);
+            respuesta.put("error", false);
+            respuesta.put("colaboradores",colaboradores);
+        } else {
+           respuesta.put("Error", true);
+            switch(respuestaAPI.getCodigo()){
+                    case Constantes.ERROR_MALFORMED_URL:
+                        respuesta.put("mensaje",(String.valueOf(Constantes.MSJ_ERROR_PETICION)));
+                        break;
+
+                    case Constantes.ERROR_PETICION:
+                        respuesta.put("mensaje",((String.valueOf(Constantes.MSJ_ERROR_PETICION))));
+                        break;
+                    default:
+                        respuesta.put("mensaje",("Lo sentimos, hay problemas para verificar sus credenciales en este momento, por favor intentelo más tarde."));
+                } 
+        }
+        return respuesta;
+    }
+    
+    
     public static Respuesta registrar(Colaborador colaborador) {
         Respuesta respuesta = new Respuesta();
         String URL = Constantes.URL_WS + "colaborador/registrar";
